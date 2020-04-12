6e74d3145319e22d7990ed2e6e57251bff34de2e
Damish <damishs88@gmail.com>
2020-04-12 12:21:23 +0530

    First Commit
    Upload all Files

---
+++ app/Controllers/MainController.js
@@ -0,0 +1,53 @@
+app.controller('MainController', ['$scope', $scope => {
+
+    $scope.list = [];
+
+    $scope.count = 0;
+    $scope.addItem = () => {
+
+        if( $scope.eventName && $scope.eventDate && $scope.eventTime && $scope.eventVenue !== ""  ){
+
+                $scope.count += 1;
+
+                $scope.list.push({
+                     id: $scope.eventDate.toLocaleDateString() + "- 0" +$scope.count,
+                     eName: $scope.eventName ,
+                     eDate: $scope.eventDate.toLocaleDateString() ,
+                     eTime: $scope.eventTime.toLocaleTimeString() ,
+                     eVenue: $scope.eventVenue ,
+                     complete: false
+                });
+
+        };
+
+        $scope.eventName="";
+        $scope.eventDate = "";
+        $scope.eventTime= "";
+        $scope.eventVenue= "";
+
+    };
+
+    $scope.showMe = false;
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
+    }
+
+    $scope.showMe2 = false;
+    $scope.showTable = function() {
+        $scope.showMe2 = !$scope.showMe2;
+    }
+
+    $scope.removeItem = () => {
+        let oldList = $scope.list;
+        $scope.list = [];
+        angular.forEach(oldList, (checked) => {
+            if (!checked.done) $scope.list.push(checked);
+        });
+    }
+}]);
+
+
+
+
+
+
